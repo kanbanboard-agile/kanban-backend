a966c82765660c731cd360da8aafb79d41a96725
prabutama <isaprabutama@gmail.com>
2025-03-10 13:13:37 +0700

    add auth middleware [skip ci]

--- src/routes/taskRoute.js
+++ src/routes/taskRoute.js
@@ -1,13 +1,18 @@
-import express from 'express';
-import TaskController from '../controllers/taskController.js';
+import express from "express";
+import TaskController from "../controllers/taskController.js";
+import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 // Rute untuk Task
-router.post('/tasks', TaskController.createTask); // Membuat tugas baru
-router.get('/tasks/:id', TaskController.getTaskById); // Mendapatkan detail tugas
-router.get('/workspaces/:workspaceId/tasks', TaskController.getWorkspaceTasks);
-router.put('/tasks/:id', TaskController.updateTask); // Memperbarui tugas
-router.delete('/tasks/:id', TaskController.deleteTask); // Menghapus tugas
+router.post("/tasks", authMiddleware, TaskController.createTask); // Membuat tugas baru
+router.get("/tasks/:id", authMiddleware, TaskController.getTaskById); // Mendapatkan detail tugas
+router.get(
+  "/workspaces/:workspaceId/tasks",
+  authMiddleware,
+  TaskController.getWorkspaceTasks
+);
+router.put("/tasks/:id", authMiddleware, TaskController.updateTask); // Memperbarui tugas
+router.delete("/tasks/:id", authMiddleware, TaskController.deleteTask); // Menghapus tugas
 
 export default router;
--- src/routes/workspaceRoute.js
+++ src/routes/workspaceRoute.js
@@ -1,13 +1,30 @@
-import express from 'express';
-import WorkspaceController from '../controllers/workspaceController.js'; 
+import express from "express";
+import WorkspaceController from "../controllers/workspaceController.js";
+import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 // Rute untuk Workspace
-router.post('/workspaces', WorkspaceController.createWorkspace); // Membuat workspace baru
-router.get('/workspaces/:id', WorkspaceController.getWorkspaceById); // Mendapatkan detail workspace
-router.get('/users/:userId/workspaces', WorkspaceController.getUserWorkspaces); // Mendapatkan semua workspace milik user
-router.put('/workspaces/:id', WorkspaceController.updateWorkspace); // Memperbarui workspace
-router.delete('/workspaces/:id', WorkspaceController.deleteWorkspace); // Menghapus workspace
+router.post("/workspaces", authMiddleware, WorkspaceController.createWorkspace); // Membuat workspace baru
+router.get(
+  "/workspaces/:id",
+  authMiddleware,
+  WorkspaceController.getWorkspaceById
+); // Mendapatkan detail workspace
+router.get(
+  "/users/:userId/workspaces",
+  authMiddleware,
+  WorkspaceController.getUserWorkspaces
+); // Mendapatkan semua workspace milik user
+router.put(
+  "/workspaces/:id",
+  authMiddleware,
+  WorkspaceController.updateWorkspace
+); // Memperbarui workspace
+router.delete(
+  "/workspaces/:id",
+  authMiddleware,
+  WorkspaceController.deleteWorkspace
+); // Menghapus workspace
 
 export default router;
