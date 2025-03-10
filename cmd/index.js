d76e36c140f5c07d3b1e01d03cfa0151038823d9
HazelMpR <hazelmpr368@gmail.com>
2025-03-10 15:11:40 +0700

    add logic aws s3

--- cmd/index.js
+++ cmd/index.js
@@ -4,6 +4,7 @@ import workspaceRoute from '../src/routes/workspaceRoute.js';
 import taskRoute from '../src/routes/taskRoute.js';
 import notificationRoute from '../src/routes/notificationRoute.js';
 import task_attachmentsRoute from '../src/routes/task_attachmentsRoute.js';
+import uploadRoute from '../src/routes/uploadRoutes.js';
 
 const app = express();
 const PORT = 3000;
@@ -17,6 +18,7 @@ app.use('/api', workspaceRoute);
 app.use('/api', taskRoute);
 app.use('/api', notificationRoute);
 app.use('/api', task_attachmentsRoute);
+app.use('/api', uploadRoute);
 
 // Menjalankan server
 app.listen(PORT, () => {
